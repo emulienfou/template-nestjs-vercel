f0a75cb7ad5c044d310cc168c1f3b8187d227131
David Sanchez <david38sanchez@gmail.com>
2024-03-18 08:51:39 -0400

    Update to use Mikro-ORM

--- process.d.ts
+++ process.d.ts
@@ -4,5 +4,11 @@ declare namespace NodeJS {
     MONGO_DB: string;
     NODE_ENV: string;
     PORT: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+    DB_SSL: string;
   }
 }
