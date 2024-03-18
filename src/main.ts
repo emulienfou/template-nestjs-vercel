973a4ede875a69132f06c9e9a81fb03329d3b0d1
David Sanchez <david38sanchez@gmail.com>
2024-03-18 09:30:52 -0400

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,48 @@
 import 'dotenv/config';
+import { ValidationError } from '@nestjs/class-validator';
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { Logger, PreconditionFailedException, ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFile } from 'node:fs/promises';
 
 import { AppModule } from './app.module';
-import { mainConfig } from './utils/mainConfig';
+import { AllExceptionsHandler } from './utils/AllExceptionsHandler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const logger = new Logger(bootstrap.name);
 
-  mainConfig(app);
+  app.enableShutdownHooks();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      enableDebugMessages: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        throw new PreconditionFailedException(validationErrors);
+      },
+    }),
+  );
+
+  const httpRef = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new AllExceptionsHandler(httpRef.httpAdapter.getHttpServer()),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS example')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {});
+
+  if (process.env.NODE_ENV === 'dev')
+    writeFile('public/swagger.json', JSON.stringify(document));
 
   await app.listen(process.env.PORT || 3000, async () => {
     const url = await app.getUrl();
