3a27028ff244d7be8b8f62a961bebcabf4c97c76
David Sanchez <david38sanchez@gmail.com>
2024-03-18 12:59:36 -0400

    Use EntityManager

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,17 @@
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
 
 import { SendMessageDTO } from './dto/send-message.dto';
+import { Author } from './entities/Author';
 
 @Injectable()
 export class AppService {
+  private readonly authorRepository: EntityRepository<Author>;
+
+  constructor(em: EntityManager) {
+    this.authorRepository = em.fork().getRepository(Author);
+  }
+
   sendMessage = (dto: SendMessageDTO) => {
     return dto;
   };
