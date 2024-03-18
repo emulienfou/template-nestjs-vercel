ec1b8eeaccefaebe342a674b506dbccd8f6e5255
David Sanchez <david38sanchez@gmail.com>
2024-03-18 14:52:57 -0400

    Test inject

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
-import { join } from 'node:path';
-
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Author } from './entities/Author';
+import config from './mikro-orm.config';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     MikroOrmModule.forFeature({ entities: [Author] }),
-    MikroOrmModule.forRoot(),
+    // @ts-ignore
+    MikroOrmModule.forRoot(config),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
